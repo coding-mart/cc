318b291014276fbc29361ec6e53ad68a69a62e48
coding-mart <69900149+coding-mart@users.noreply.github.com>
2020-10-17 10:58:43 +0500

    Add files via upload

---
+++ lab2/lab 2.py
@@ -0,0 +1,55 @@
+#########lab 2 ####
+if __name__ == "__main__":
+    cities = ["islamabad", "krachi", "lahore", "krachi", "taxila", 12, 12.5 ]
+    #lis have also negitive index
+    #           0  -6      1-5        2-4      3-3       4-2   5 -1
+    #append add value at the end of list
+    #cities.append("abtabodt")
+    
+    #replace the value at any index
+    #cities[0]= "Muree"
+    
+    #insert at prticular value at prticular index.
+    #cities.insert(1,"GIK")
+    
+    #pop taek index number and delet the vaule.
+    #cities.pop(4)
+    
+    #remove function remove the frist occurence value in list not other value
+    #cities.remove("krachi")
+    
+    #print(cities)
+
+'''
+    i = 0
+    size = len(cities)
+    while(i<size):
+        print(cities[i])
+        i=i+1
+    for i in range(0,size):
+        print(cities[i])
+'''
+'''
+for x in cities:
+        print(x)
+    #print(size)
+'''
+'''
+a= "int name = abc"
+for x in a:
+    print(a)
+'''
+'''
+#nested list like 2D array
+a= [12, "abc", [33, 45], 34]
+print(a[2][1])
+'''
+
+'''
+###############tuple#######
+#we cant change the value of tupele after initiliztion
+days= ( "sunday", "Monday", 12,34)
+
+print (days)
+'''
+###sets     and dectionary data structue ######
