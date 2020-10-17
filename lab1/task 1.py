1f463044dbe4990fdf3ce50226e510c4d7d16d89
coding-mart <69900149+coding-mart@users.noreply.github.com>
2020-10-17 10:57:38 +0500

    Add files via upload

---
+++ lab1/task 1.py
@@ -0,0 +1,50 @@
+if __name__=="__main__": #basics of python
+   '''frist_number = 45
+   second_number = 67
+   sum = frist_number+second_number 
+   print ("sum is",sum)'''
+        
+# condition
+'''
+fn = 34
+sn = 23
+sum = fn + sn
+           
+if (sum >= 80 and sum <= 100):
+    print("Good value")
+    print("very Good")
+    if(True):
+        print("Greate")
+elif(sum >= 70 and sum <80):
+    print("B grade")
+else:
+    print("bad value")
+    print("Bye")'''
+
+#loops
+#while loop
+''' i=0
+   while( i<5):
+       print("value ",i)
+       i = i+1'''
+  #for loop
+  #range(start , end ,change)
+#range(start,end) incremtn 1 auto
+#range(end) tell only end
+'''
+for i in range(0,10,2):
+      print("value ", i)'''
+
+#lab task
+'''
+input a positive number from user and then print
+following pattren
+useing while and for
+print error msg if user enter e.g -6
+supose user enter 5
+*****
+**  *
+* * *
+*  **
+*****
+'''
